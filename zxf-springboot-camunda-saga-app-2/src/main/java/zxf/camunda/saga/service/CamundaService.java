8e36b62d8c57451ac2a9b87adce810b9f3523f5a
Davis ZAN <zanxiaofeng@163.com>
2024-09-05 20:02:31 -0500

    update

--- zxf-springboot-camunda-saga-app-2/src/main/java/zxf/camunda/saga/service/CamundaService.java
+++ zxf-springboot-camunda-saga-app-2/src/main/java/zxf/camunda/saga/service/CamundaService.java
@@ -6,10 +6,12 @@ import org.camunda.bpm.model.xml.instance.ModelElementInstance;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Slf4j
 @Component
 public class CamundaService {
-    //In order to check the first call, the camunda.bpm.default-number-of-retries must be set to a special value.
+    //In order to check the first call, the camunda.bpm.default-number-of-retries must be set to a large number.
     @Value("${camunda.bpm.default-number-of-retries}")
     private int initialRetryNumber;
 
@@ -36,19 +38,27 @@ public class CamundaService {
     }
 
     private int getLeftRetryTimes(DelegateExecution execution) {
-        return execution.getProcessEngine().getManagementService().createJobQuery()
+        int leftRetryTimes = execution.getProcessEngine().getManagementService().createJobQuery()
                 .executionId(execution.getId()).singleResult().getRetries();
+        if (leftRetryTimes > initialRetryNumber) {
+            throw new IllegalStateException(String.format("The default retry number must be set to a large number. initial=%d, left=%d", initialRetryNumber, leftRetryTimes));
+        }
+        return leftRetryTimes;
     }
 
     private int getTotalRetryCount(DelegateExecution execution) {
-        ModelElementInstance failedJobRetryTimeCycleEle = execution.getBpmnModelElementInstance().getExtensionElements()
-                .getUniqueChildElementByNameNs("http://camunda.org/schema/1.0/bpmn", "failedJobRetryTimeCycle");
-        if (failedJobRetryTimeCycleEle == null) {
-            return initialRetryNumber;
+        Optional<Integer> failedJobRetryCount = Optional.ofNullable(execution.getBpmnModelElementInstance().getExtensionElements())
+                .map(ele -> ele.getUniqueChildElementByNameNs("http://camunda.org/schema/1.0/bpmn", "failedJobRetryTimeCycle"))
+                .map(ModelElementInstance::getTextContent)
+                .map(this::parseRetryTimes);
+        int totalRetryCount = failedJobRetryCount.map(failedRetryCount -> failedRetryCount == 0 ? 1 : failedRetryCount).orElseGet(() -> initialRetryNumber);
+        if (totalRetryCount > initialRetryNumber) {
+            throw new IllegalStateException(String.format("The default retry number must be set to a large number. initial=%d, total=%d", initialRetryNumber, totalRetryCount));
         }
+        return totalRetryCount;
+    }
 
-        String failedJobRetryTimeCycle = failedJobRetryTimeCycleEle.getTextContent();
-        int failedJobRetryRetryCount = Integer.parseInt(failedJobRetryTimeCycle.substring(1, failedJobRetryTimeCycle.indexOf("/")));
-        return failedJobRetryRetryCount == 0 ? 1 : failedJobRetryRetryCount;
+    private Integer parseRetryTimes(String failedJobRetryTimeCycle) {
+        return Integer.parseInt(failedJobRetryTimeCycle.substring(1, failedJobRetryTimeCycle.indexOf("/")));
     }
 }
