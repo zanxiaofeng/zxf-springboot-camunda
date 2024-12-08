75c79b067ff531f3564778378e4e86d9a7ad7474
Davis ZAN <zanxiaofeng@163.com>
2024-12-08 19:18:42 +0800

    update

--- zxf-springboot-camunda-arch-app/src/main/java/zxf/camunda/arch/app/delegate/HttpRequestDelegate.java
+++ zxf-springboot-camunda-arch-app/src/main/java/zxf/camunda/arch/app/delegate/HttpRequestDelegate.java
@@ -1,10 +1,12 @@
 package zxf.camunda.arch.app.delegate;
 
 import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import zxf.camunda.arch.app.service.CamundaService;
 
 @Slf4j
@@ -16,5 +18,11 @@ public class HttpRequestDelegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution) throws Exception {
         execution.setVariable("paymentStatus", "OK");
+
+        if (StringUtils.hasText((String) execution.getVariable("errorInfo"))) {
+            String error = (String) execution.getVariable("errorInfo");
+            execution.removeVariable("errorInfo");
+            throw new BpmnError(error, error);
+        }
     }
 }
---
+++ zxf-springboot-camunda-arch-app/src/main/java/zxf/camunda/arch/app/delegate/NotificationDelegate.java
@@ -0,0 +1,23 @@
+package zxf.camunda.arch.app.delegate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class NotificationDelegate implements JavaDelegate {
+    @Autowired
+    ProcessEngine processEngine;
+
+    @Override
+    public void execute(DelegateExecution execution) throws JsonProcessingException {
+        String message = (String) execution.getVariable("message");
+        log.info("notification, {}", message);
+        processEngine.getRuntimeService().correlateMessage("PaymentProcess.Notification", execution.getBusinessKey());
+    }
+}
